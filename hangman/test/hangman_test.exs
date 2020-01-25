0ebe86c2a2a00806a59cc05f8611d6f6a780ab22
markshawtoronto <mshaw@pagerduty.com>
2020-01-25 11:26:52 -0500

    Fixing tests.

--- hangman/test/hangman_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HangmanTest do
-  use ExUnit.Case
-  doctest Hangman
-
-  test "greets the world" do
-    assert Hangman.new_game == %{turns_left: 7, game_state: :initializing}
-  end
-end
